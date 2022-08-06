82c72e8c0db34dbdcba15de67cc63a43ede35969
Alejandro Dotor <alejandrodotor8@gmail.com>
2022-08-05 21:49:02 -0500

    ENV port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routerApi = require('./routes');
 
 //Init Express
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log('Listening at: http://localhost:' + port);
